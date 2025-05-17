92f1236351e93b7ab5e31510130a9356b3256a32
Rick—C173 <rick_C173@qq.com>
2025-05-17 14:28:17 +0800

    first

---
+++ src/test/java/com/example/jiaowu/JiaowuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jiaowu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiaowuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
